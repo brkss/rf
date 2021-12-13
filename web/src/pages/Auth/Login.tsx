81a8fff7b7f628985da64c62c11eea5455976d24
Brahim Berkasse <berkassebrahim@gmail.com>
2021-12-13 16:44:17 +0100

    🏗 setup login page 1

---
+++ web/src/pages/Auth/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Center, Heading } from "@chakra-ui/react";
+
+export const Login: React.FC = () => {
+  return (
+    <Box h={"100vh"}>
+      <Center h={"100%"}>
+        <Heading>Login Page</Heading>
+      </Center>
+    </Box>
+  );
+};
--- web/src/utils/config/routes.ts
+++ web/src/utils/config/routes.ts
@@ -1,5 +1,5 @@
 import { IRoute } from "../types/Route";
-import { Home } from "../../pages";
+import { Home, Login } from "../../pages";
 
 export const routes: IRoute[] = [
   {
@@ -7,5 +7,11 @@ export const routes: IRoute[] = [
     path: "/",
     exact: true,
     component: Home,
+  },
+  {
+    name: "Login",
+    path: "/login",
+    exact: true,
+    component: Login,
   },
 ];
