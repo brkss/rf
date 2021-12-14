8393494f3132b72c3378266524f1f77827421915
Brahim Berkasse <berkassebrahim@gmail.com>
2021-12-14 18:12:45 +0100

    👁  refresh access token on refresh

--- web/src/Application.tsx
+++ web/src/Application.tsx
@@ -6,8 +6,36 @@ import {
   RouteComponentProps,
 } from "react-router-dom";
 import { routes } from "./utils/config/routes";
+import { DEFAULT_API_URL } from "./utils/config/constants";
+import { getAccessToken, setAccessToken } from "./utils/token/token";
+import { Center, Spinner } from "@chakra-ui/react";
 
 export const Application: React.FC = () => {
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch(`${DEFAULT_API_URL}/refresh_token`, {
+      credentials: "include",
+      method: "POST",
+    }).then(async (res) => {
+      const data = await res.json();
+      if (data.status === true) {
+        setAccessToken(data.token);
+        console.log("access token", getAccessToken());
+      }
+      console.log("refresh token result => ", data);
+
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading)
+    return (
+      <Center height="100vh">
+        <Spinner size="md" />
+      </Center>
+    );
+
   return (
     <>
       <BrowserRouter>
---
+++ web/src/generated/graphql.tsx
@@ -0,0 +1,77 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions =  {}
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type AuthDefaultResponse = {
+  __typename?: 'AuthDefaultResponse';
+  message?: Maybe<Scalars['String']>;
+  status: Scalars['Boolean'];
+  token?: Maybe<Scalars['String']>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  auth: AuthDefaultResponse;
+};
+
+
+export type MutationAuthArgs = {
+  code: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  ping: Scalars['String'];
+};
+
+export type PingQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type PingQuery = { __typename?: 'Query', ping: string };
+
+
+export const PingDocument = gql`
+    query Ping {
+  ping
+}
+    `;
+
+/**
+ * __usePingQuery__
+ *
+ * To run a query within a React component, call `usePingQuery` and pass it any options that fit your needs.
+ * When your component renders, `usePingQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = usePingQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function usePingQuery(baseOptions?: Apollo.QueryHookOptions<PingQuery, PingQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<PingQuery, PingQueryVariables>(PingDocument, options);
+      }
+export function usePingLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<PingQuery, PingQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<PingQuery, PingQueryVariables>(PingDocument, options);
+        }
+export type PingQueryHookResult = ReturnType<typeof usePingQuery>;
+export type PingLazyQueryHookResult = ReturnType<typeof usePingLazyQuery>;
+export type PingQueryResult = Apollo.QueryResult<PingQuery, PingQueryVariables>;
