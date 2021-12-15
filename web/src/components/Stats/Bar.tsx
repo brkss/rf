09c7bf0b1ccc980581cecfa500abfb6c6346c49f
Brahim Berkasse <berkassebrahim@gmail.com>
2021-12-15 17:44:46 +0100

    📊 stats Bar UI !

--- web/src/components/Stats/Bar.tsx
+++ web/src/components/Stats/Bar.tsx
@@ -3,10 +3,18 @@ import { Box, Text } from "@chakra-ui/react";
 
 export const Bar: React.FC = () => {
   return (
-    <Box>
-      <Text>👩‍🚒</Text>
-      <Box w={"20px"} height={"50px"} bg={"#FFB3C8"} />
-      <Text>68%</Text>
+    <Box textAlign={"center"}>
+      <Text fontSize={"25px"}>😁</Text>
+      <Box
+        margin={"auto"}
+        borderRadius={"50px"}
+        w={"20px"}
+        height={"50px"}
+        bg={"#FFB3C8"}
+      />
+      <Text fontWeight={"bold"} fontSize={"15px"} marginTop={"7px"}>
+        68%
+      </Text>
     </Box>
   );
 };
