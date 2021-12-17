357ccbfcde167ac8dafa3ead03301f1ecdd12924
Brahim Berkasse <berkassebrahim@gmail.com>
2021-12-17 18:57:04 +0100

    👁 display timer on not current meal !

--- web/src/pages/FeedBack.tsx
+++ web/src/pages/FeedBack.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Center, Spinner } from "@chakra-ui/react";
 import { Reactions, Bars, Timer } from "../components";
+import { useMealTimeQuery } from "../generated/graphql";
 
 export const FeedBack: React.FC = () => {
+  const _mealTime = useMealTimeQuery();
+
+  if (_mealTime.loading)
+    return (
+      <Center h={"100vh"}>
+        <Spinner />
+      </Center>
+    );
   return (
     <Box>
-      <Timer />
-      <Reactions />
+      {_mealTime.data!.mealTime!.is_current ? <Reactions /> : <Timer />}
       <Bars />
     </Box>
   );
